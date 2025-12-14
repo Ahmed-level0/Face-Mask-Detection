3b048a8a163ccbf66cf71103511dfef3c499e970
ahmed <ahmedhambuta9@gmail.com>
2025-12-14 22:11:06 +0200

    first commit

---
+++ Python Scripts/Real_Time_Detection.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("Models/Face_mask_model.pt")
+
+cap = cv2.VideoCapture(0)
+
+while True: 
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame, conf=0.5)
+
+    annotated_frame = results[0].plot()
+    cv2.imshow("Face Mask Detection", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
